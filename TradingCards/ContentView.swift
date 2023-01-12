9523874d168eadb1c972438eb254e5f0f4fdfba8
Aidan Kang <akang@lcs.on.ca>
2023-01-12 11:53:07 -0500

    Added initial design with image and name

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -9,18 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            VStack {
+                Text("JAMES")
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Text("MADDISON")
+                    .foregroundColor(.white)
+                    .fontWeight(.heavy)
+                    .font(.title)
+                Spacer()
+                    .frame(height: 680)
+            }
+            
+            VStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 300, height: 500)
+                    .foregroundColor(.white)
+                Spacer()
+                    .frame(height: 120)
+            }
+            VStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 260, height: 460)
+                    .foregroundColor(.blue)
+                Spacer()
+                    .frame(height: 120)
+            }
+            VStack {
+                Image("JamesMaddison")
+                    .resizable()
+                    .frame(width:240, height:440)
+                Spacer()
+                    .frame(height:120)
+            }
+            
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
